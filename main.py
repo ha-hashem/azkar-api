416bed402f17fa8d896209a74fdafc7871f55ae9
Hashem Ahmed <68099570+ha-hashem@users.noreply.github.com>
2021-09-26 10:02:33 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,27 @@
+# data from https://github.com/osamayy/azkar-db
+
+from random import randint
+from csv import reader
+from flask import Flask
+
+f = open('azkar.csv', 'r')
+content = list(reader(f))
+f.close()
+
+content = content[1:]
+
+app = Flask(__name__)
+
+# generate from 0 to len(content) - 1
+
+@app.route('/')
+def index():
+	random_int = randint(0, len(content)-1)	
+	gen = {
+		'category' : content[random_int][0],
+		'zekr' : content[random_int][1],
+		'description': content[random_int][2],
+		'count': content[random_int][3],
+		'reference': content[random_int][4]
+	}
+	return gen
